588f8e1f1b08bb0ed8bc274d56c4285c0653a6ff
eerrbis@gmail.com <trimurs@gmail.com>
2023-05-31 03:58:43 +0300

    добавил пикчи + переделал шаблон

--- products/views.py
+++ products/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from products.models import Product, ProductCategory
+
+
 def index(request):
     context={'title':'store'}
     return render(request, 'products/index.html',context=context)
 
 def products(request):
-    context = {'title': 'store-catalog'}
-    return render(request, 'products/products.html')
+    context = {'title': 'store-catalog',
+               'products': Product.objects.all(),
+               'categories': ProductCategory.objects.all()}
+    return render(request, 'products/products.html',context)
--- store/settings.py
+++ store/settings.py
@@ -122,5 +122,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR/'static',]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
