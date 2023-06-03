69526f3fc4e0df7c45116c4472bf1aa9013ee9da
eerrbis@gmail.com <trimurs@gmail.com>
2023-06-03 04:41:06 +0300

    добавил, чтобы оформление подружалось в форму регистрации + регистрацию

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 
 from users.models import User
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 from django.contrib import auth
 
 # Create your views here.
@@ -21,4 +21,11 @@ def login(request):
     context = {'form' : form}
     return render(request,'users/login.html',context)
 def registration(request):
-    return render(request,'users/register.html')
+    if request.method=='POST':
+        form=UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+    form=UserRegistrationForm()
+    context={'form':form}
+    return render(request,'users/register.html', context)
