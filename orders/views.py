aac7a99a3cf2f3d5136d585df6b304e02092f772
eerrbis@gmail.com <trimurs@gmail.com>
2023-07-16 04:55:11 +0300

    Привязка orders формы к шаблону

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,15 @@
+from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+
+from common.views import CommonMixin
 from orders.forms import OrderForm
 
-class OrderCreate(CreateView):
+class OrderCreate(CommonMixin, CreateView):
     template_name = 'orders/order-create.html'
     form_class = OrderForm
+    title = 'Оформление заказа'
+    success_url = reverse_lazy('orders:order_create')
+
+    def form_valid(self, form):
+        form.instance.initiator = self.request.user
+        return super(OrderCreate, self).form_valid(form)
