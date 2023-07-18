f1898cfb0b393575f67e6ca89128b1c0b88bb0c6
eerrbis@gmail.com <trimurs@gmail.com>
2023-07-19 01:05:16 +0300

    добавлена оплата через stripe

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from orders.views import OrderCreate
+from orders.views import OrderCreate, SuccessTemplateView,CancelTemplateView
 
 app_name = 'orders'
 urlpatterns = [
     path('create/', OrderCreate.as_view(), name='order_create'),
+    path('order-success',SuccessTemplateView.as_view(), name='order_success'),
+    path('order-cancel', CancelTemplateView.as_view(), name='order_cancel')
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,14 +1,51 @@
-from django.urls import reverse_lazy
+from http import HTTPStatus
+
+from django.urls import reverse_lazy, reverse
+from django.views.generic import TemplateView
 from django.views.generic.edit import CreateView
+from django.http import HttpResponseRedirect
+
 
 from common.views import CommonMixin
 from orders.forms import OrderForm
+
+import stripe
+
+from store import settings
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+
+class SuccessTemplateView(CommonMixin, TemplateView):
+    template_name = 'orders/success.html'
+    title = 'Спасибо за заказ'
+
+class CancelTemplateView(CommonMixin, TemplateView):
+    template_name = 'orders/cancel.html'
+
 
 class OrderCreate(CommonMixin, CreateView):
     template_name = 'orders/order-create.html'
     form_class = OrderForm
     title = 'Оформление заказа'
     success_url = reverse_lazy('orders:order_create')
+
+    def post(self, request, *args, **kwargs):
+        super(OrderCreate, self).post(self, request, *args, **kwargs)
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                    'price': 'price_1NVLaJFZIPJsHLDcb3ub8bhe',
+                    'quantity': 1,
+                },
+            ],
+            mode='payment',
+            success_url='{}{}'.format(settings.DOMAIN_NAME, reverse('orders:order_success')),
+            cancel_url='{}{}'.format(settings.DOMAIN_NAME, reverse('orders:order_cancel')),
+        )
+        return HttpResponseRedirect(checkout_session.url,status=HTTPStatus.SEE_OTHER)
+
 
     def form_valid(self, form):
         form.instance.initiator = self.request.user
